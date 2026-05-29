6712d6ac4aad1a0f824f7feb444693b8a96cd924
zircono16i-eng <zircono16i@gmail.com>
2026-05-29 13:55:31 +0700

    Create intro.php

---
+++ intro.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ฉันชื่อว่า <?php echo "ปิยังกูร ทิพยโท"; ?></h1>
+</body>
+</html>
